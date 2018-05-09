eea4b02ed8656b35c42bdfd188e650e7a70ed6a0
stebunoff <33266476+stebunoff@users.noreply.github.com>
2018-05-09 13:09:25 +0300

    test

--- templates/index.php
+++ templates/index.php
@@ -30,18 +30,18 @@
         <?php foreach ($template_data['ads'] as $key => $item): ?>
         <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$item['url']; ?>" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=htmlspecialchars($item['url']); ?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$item['category']; ?></span>
+                    <span class="lot__category"><?=htmlspecialchars($item['category']); ?></span>
                     <h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($item['title']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=format_price(htmlspecialchars($item['price'])); ?></span>
+                            <span class="lot__cost"><?=htmlspecialchars(format_price($item['price'])); ?></span>
                         </div>
                         <div class="lot__timer timer">
-
+                            <?=intdiv(time_to_expire(), $template_data['sec_in_hour']) . ':' . intdiv((time_to_expire() % $template_data['sec_in_hour']), $template_data['sec_in_min']); ?>
                         </div>
                     </div>
                 </div>
