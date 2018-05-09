513a7c38dce46e5c8e794b5ba349ac4b65d778c3
stebunoff <33266476+stebunoff@users.noreply.github.com>
2018-05-09 14:13:43 +0300

    Скорректировал расчёт и вынес функцию в файл с функциями

--- functions.php
+++ functions.php
@@ -9,4 +9,14 @@ function renderTemplate ($template_path, $template_data) {
     $content = ob_get_clean();
     return $content;
 }
+
+function time_to_expire () {
+    date_default_timezone_set('Europe/Moscow');
+    $sec_in_hour = 3600;
+    $sec_in_min = 60;
+    $exp_time = strtotime('tomorrow');
+    $curr_time = strtotime('now');
+    $time_to_exp = floor(($exp_time - $curr_time)/$sec_in_hour) . ":" . floor(($exp_time - $curr_time) % $sec_in_hour / $sec_in_min);
+    return $time_to_exp;
+}
 ?>
